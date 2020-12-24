0811693b4b66e61a3450a1a570f7f664c7f47cbc
Julien <ares11@outlook.fr>
2020-12-24 10:57:12 +0100

    [fix] Delete folder for Cleaner

--- services/Cleaner.js
+++ services/Cleaner.js
@@ -1,3 +1,5 @@
+const rimraf = require("rimraf");
+
 class Cleaner {
     static checkExist(fs, fileOrFolderPath) {
         return new Promise((resolve, reject) => {
@@ -24,12 +26,11 @@ class Cleaner {
     static deleteFolder(fs, folderPath) {
         return new Promise(async (resolve, reject) => {
             if (await this.checkExist(fs, folderPath)){
-                fs.rmdir(folderPath, { recursive: true }, function (err) {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve();
-                })
+                rimraf(folderPath, function (err) 
+                { 
+                    if (err) return reject(err);
+                    resolve() 
+                });
             }
         });
     }
